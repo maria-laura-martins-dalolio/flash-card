53824e8e0e606188f99b0fbc6305459513e7dde7
maria-laura-martins-dalolio <160801843+maria-laura-martins-dalolio@users.noreply.github.com>
2024-10-23 21:50:40 -0300

    projeto

---
+++ perguntas.js
@@ -0,0 +1,8 @@
+criacartao(
+    'programacao',
+    'o que é CSS',
+'o css é uma linguagem de estilizacao')
+criacartao(
+    'matematica',
+    'quantos é 5 X 3 + 4 X 7.',
+    'o resultado é 43.')
